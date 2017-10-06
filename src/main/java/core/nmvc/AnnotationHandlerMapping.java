3adb282b120a7d683609f548a7c5c9c03a8131d7
SJH-PC\SJH <hidden110585@gmail.com>
2017-10-06 12:18:32 +0900

    commit

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package core.nmvc;
 
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.google.common.collect.Maps;
+import org.reflections.ReflectionUtils;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import core.annotation.ControllerScanner;
+import core.annotation.RequestMapping;
 import core.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
@@ -18,9 +25,29 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-
+    	ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+    	Map<Class<?>, Object> controllers = controllerScanner.getcontrollers();
+    	
+    	Set<Method> methods = getRequestMappingMethods(controllers.keySet());
+    	
+    	for (Method method : methods) {
+    		RequestMapping rm = method.getAnnotation(RequestMapping.class);
+    		handlerExecutions.put(createHandlerKey(rm), new HandlerExecution(controllers.get(method.getDeclaringClass()), method));
+    		}
     }
 
+    private HandlerKey createHandlerKey(RequestMapping rm) {
+    	return new HandlerKey(rm.value(), rm.method());
+    }
+
+    private Set<Method> getRequestMappingMethods(Set<Class<?>>controllers) {
+    	Set<Method> requestMappingMethods = Sets.newHashSet();
+    	for (Class<?> clazz : controllers) {
+    		requestMappingMethods.addAll(ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class)));
+    	}
+    	return requestMappingMethods;
+    }
+    
     public HandlerExecution getHandler(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
--- src/main/java/core/nmvc/HandlerExecution.java
+++ src/main/java/core/nmvc/HandlerExecution.java
@@ -1,12 +1,29 @@
 package core.nmvc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import core.mvc.ModelAndView;
 
 public class HandlerExecution {
-    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+	private Object declaredObject;
+	private Method method;
+	
+	public HandlerExecution(Object declaredObject, Method method) {
+		this.declaredObject = declaredObject;
+		this.method = method;
+	}
+
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	try {
+			return (ModelAndView)method.invoke(declaredObject, request, response);
+		} catch (IllegalAccessException | IllegalArgumentException |
+				InvocationTargetException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
     }
 }
